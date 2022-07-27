92aa0020c3015b9e708f203d04e1338bfa2da700
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-07-26 19:54:27 -0700

    Started work on spells

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 # -----------------------------------------------------------
 #                         Imports
 # -----------------------------------------------------------
+import asyncio
 import os
 
 from enum import Enum
-
-from src.structures.documentModel import Test
+from data.dnd5e.models.SpellModel import DnD5eSpell
 
 
 # -----------------------------------------------------------
@@ -27,9 +27,13 @@ class App:
     async def start(self) -> None:
         """ Start the Application """
         # Get Json files
-        filepath: str = self._get_filepath()
+        # filepath: str = self._get_filepath()
+        filepath: str = './data/dnd5e/test/'
         files: list[str] = [f'{filepath}{file}' for file in os.listdir(
             filepath) if file.endswith('.json')]
+
+        docs = [DnD5eSpell(await DnD5eSpell.load_json(f)) for f in files]
+        print(docs[0].get_system_data())
 
     def _get_filepath(self) -> str:
         while True:
@@ -38,6 +42,8 @@ class App:
                 break
             else:
                 print('Unable to find the specified path. Try again.')
+
+        return filepath
 
 
 # -----------------------------------------------------------
